1b08dd4585df16c9bd252ce0a64d96cde175fcbc
gigglesbiggles <192318580+gigglesbiggles@users.noreply.github.com>
2025-02-04 10:22:39 +0800

    added v2f

--- newfile.c
+++ newfile.c
@@ -1,4 +1,18 @@
+typedef struct
+{
+  float x, y;
+} v2f;
+
+inline static v2f v2f_add(v2f a, v2f b)
+{
+  return (v2f)
+  {
+    a.x + b.x,
+    a.y + b.y
+  };
+}
+
 int main(void)
 {
-	return 0;
+  return 0;
 }
